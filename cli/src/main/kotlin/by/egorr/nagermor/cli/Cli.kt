73688a5ebc7d0e89432e3cd733638ac6bd9df483
Yahor Berdnikau <egorr.berd@gmail.com>
2020-10-10 15:34:21 +0200

    Add CLI '--classpath' option.

--- cli/src/main/kotlin/by/egorr/nagermor/cli/Cli.kt
+++ cli/src/main/kotlin/by/egorr/nagermor/cli/Cli.kt
@@ -1,11 +1,25 @@
 package by.egorr.nagermor.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.split
 
 fun main(args: Array<String>) = Compile().main(args)
 
 class Compile : CliktCommand() {
+    private val classpath by option(
+        "--classpath", "-cp",
+        help = """
+            Specify paths whether to find user class files.
+            Paths should be separated by ':' character. 
+        """.trimMargin()
+    )
+        .split(":")
+        .default(emptyList())
+
     override fun run() {
         echo("Test run")
+        echo("Provided classpath: $classpath")
     }
 }
