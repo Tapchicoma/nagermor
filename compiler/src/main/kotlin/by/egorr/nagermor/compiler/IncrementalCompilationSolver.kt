d2a5e2c4adc3bec67e43a95f9bd8606e3154f46a
Yahor Berdnikau <egorr.berd@gmail.com>
2020-10-15 21:35:27 +0200

    Add incremental compilation solver api.

---
+++ compiler/src/main/kotlin/by/egorr/nagermor/compiler/IncrementalCompilationSolver.kt
@@ -0,0 +1,48 @@
+package by.egorr.nagermor.compiler
+
+import by.egorr.nagermor.abi.AbiReader
+import by.egorr.nagermor.abi.JavaAbiReader
+import java.nio.file.Files
+import java.nio.file.OpenOption
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+
+class IncrementalCompilationSolver(
+    private val incrementalCacheFile: Path,
+    private val abiReader: AbiReader = JavaAbiReader(),
+) {
+    fun sourceFileRemoved(
+        outputDir: Path,
+        sourceFile: Path
+    ): Set<Path> = emptySet()
+
+    fun sourceFileChanged(
+        outputDir: Path,
+        sourceFile: Path
+    ): Set<Path> = emptySet()
+
+    fun updateSourceFilesAbi(
+        outputDir: Path,
+        sourceFiles: Set<Path>
+    ) {
+
+    }
+
+    fun removeSourceFiles(
+        outputDir: Path,
+        sourceFiles: Set<Path>
+    ) {
+
+    }
+
+    /**
+     * Resets incremental cache state.
+     */
+    fun resetInternalState() {
+        Files.write(
+            incrementalCacheFile,
+            ByteArray(0),
+            StandardOpenOption.TRUNCATE_EXISTING
+        )
+    }
+}
